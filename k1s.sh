07d4e9f63937d4e3b207524a2ee04b1ac45253dd
Daniel Weibel <danielmweibel@gmail.com>
2020-06-24 12:38:45 +0200

    Refactor colorisation of Pod phase to separate function

--- k1s.sh
+++ k1s.sh
@@ -17,14 +17,18 @@ port=$(head -n 1 <&3 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
 
 printf Loading && for _ in 1 2 3; do printf . && sleep 0.3; done
 
+colorize() {
+    case "$1" in Running) color=32;; Pending) color=33;; *) color=31;; esac
+    printf "\033[${color}m$phase\033[0m"
+}
+
 curl -N -s "http://localhost:$port/api/v1/namespaces/$namespace/pods?watch=true" |
   while read -r line; do
     name=$(jq -r '.object.metadata.name' <<<"$line")
     phase=$(jq -r '.object.status.phase' <<<"$line")
     is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | select(.type=="Ready").status else "False" end' <<<"$line")
     [[ "$phase" = Running && "$is_ready" != True ]] && phase=Pending
-    case "$phase" in Running) color=32;; Pending) color=33;; *) color=31;; esac
-    phase=$(printf "\033[${color}m$phase\033[0m")
+    phase=$(colorize "$phase")
     case $(jq -r .type <<<"$line") in
       ADDED) echo "$name $phase" >>"$file" ;;
       MODIFIED) sed -i "s/^$name .*$/$name $phase/" "$file" ;;
