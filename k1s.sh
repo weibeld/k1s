f1f268e01578635ffd992b4e7663a3995c297045
Daniel Weibel <danielmweibel@gmail.com>
2020-06-24 17:48:45 +0200

    Add support for specifying resource type
    The resource type (e.g. pods, deployments) can be specified as the
    second command-line argument (after the namespace).
    
    At the beginning, the code makes a 'kubectl get' request for this resource
    type, which serves two purposes:
    
      1. Check if it's a valid resource type. If the resource type is invalid
         (e.g. "foo"), the 'kubectl get' command will fail and k1s will exit
         with an error message too.
    
      2. Identify the API group and version of the specified resource type.
         This information is extracted from the output of kubectl (with
         increased verbosity). This is informaiton is needed to construct
         the URL for the watch request. kubectl includes all the logic for
         identifying this information (it also caches it), so it makes sense
         to use kubectl. In fact, the entire API path is extracted from the
         kubectl output and directly used in the watch request.
    
    A consequence of the last point above is that now also aliases of
    resource type names can be specified. For example, for Pods, "pods",
    "pod", and "po" can be specified, and for Deployments, "deployments",
    "deployment", and "deploy" can be specified, and so on.

--- k1s.sh
+++ k1s.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-namespace=${1:-default}
-resource=pods
-file=$(mktemp)
+set -o pipefail
 
+namespace=${1:-default}
+resource_type=${2:-pods}
+
+# Check resource type and identify API path
+printf Loading && while true; do printf . && sleep 0.1; done &
+if ! path=$(kubectl get "$resource_type" -n "$namespace" -v 6 2>&1 >/dev/null | grep GET | tail -n 1 | sed -n 's#.*https://[^/]*\([/a-z0-9-]*\).*#\1#p'); then
+  kill -9 "$!" && echo -e "\nInvalid resource type: $resource_type" && exit 1
+fi
+kill -9 "$!" && wait "$!" 2>/dev/null
+
+# Start kubectl proxy
+exec 4< <(kubectl proxy -p 0)
+port=$(head -n 1 <&4 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
+
+file=$(mktemp)
 cat <<EOF >"$file"
  ____ ____ ____
-||k |||1 |||s ||  Kubernetes dashboard
+||k |||1 |||s ||  Kubernetes Dashboard
 ||__|||__|||__||  Namespace: $namespace
-|/__\|/__\|/__\|  Resource: $resource
+|/__\|/__\|/__\|  Resources: ${path##*/}
 
 EOF
 
-exec 3< <(kubectl proxy -p 0)
-port=$(head -n 1 <&3 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
-
-printf Loading && for _ in 1 2 3; do printf . && sleep 0.3; done
-
 colorize() {
     case "$1" in Running) color=32;; Pending) color=33;; *) color=31;; esac
-    printf "\033[${color}m$phase\033[0m"
+    printf "\033[${color}m$1\033[0m"
 }
 
-curl -N -s "http://localhost:$port/api/v1/namespaces/$namespace/pods?watch=true" |
+curl -N -s "http://localhost:$port$path?watch=true" |
   while read -r line; do
     name=$(jq -r '.object.metadata.name' <<<"$line")
-    phase=$(jq -r '.object.status.phase' <<<"$line")
-    is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | select(.type=="Ready").status else "False" end' <<<"$line")
-    [[ "$phase" = Running && "$is_ready" != True ]] && phase=Pending
-    phase=$(colorize "$phase")
+    if [[ "${path##*/}" = pods ]]; then
+      pod_info=$(jq -r '.object.status.phase' <<<"$line")
+      is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | select(.type=="Ready").status else "False" end' <<<"$line")
+      [[ "$pod_info" = Running && "$is_ready" != True ]] && pod_info=Pending
+      pod_info=$(colorize "$pod_info")
+    fi
     case $(jq -r .type <<<"$line") in
-      ADDED) echo "$name $phase" >>"$file" ;;
-      MODIFIED) sed -i "s/^$name .*$/$name $phase/" "$file" ;;
+      ADDED) echo "$name $pod_info" >>"$file" ;;
+      MODIFIED) sed -i "s/^$name .*$/$name $pod_info/" "$file" ;;
       DELETED) sed -i "/^$name .*$/d" "$file";;
     esac
   done &
