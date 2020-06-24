2e921b7f14842df1f7b33d05a9f4f2313de5eb06
Daniel Weibel <danielmweibel@gmail.com>
2020-06-24 12:22:43 +0200

    Check for readiness of Pods
    When a Pod is terminating, its Ready condition is false, but its phase
    is still Running. Now, the code checks for the status of the Ready
    condition, and if it's false, but the phase is Running, the phase is
    changed to Pending.
    
    This improves the UX when terminating Pods (e.g. scaling down a
    Deployment). The Pods that get terminated are now immediately marked as
    Pending until they are fully deleted (before, they just stayed Running
    they were fully deleted).

--- k1s.sh
+++ k1s.sh
@@ -6,7 +6,7 @@ file=$(mktemp)
 
 cat <<EOF >"$file"
  ____ ____ ____
-||k |||1 |||s ||
+||k |||1 |||s ||  Kubernetes dashboard
 ||__|||__|||__||  Namespace: $namespace
 |/__\|/__\|/__\|  Resource: $resource
 
@@ -15,17 +15,19 @@ EOF
 exec 3< <(kubectl proxy -p 0)
 port=$(head -n 1 <&3 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
 
-printf Loading && for i in 1 2 3; do printf . && sleep 0.3; done
+printf Loading && for _ in 1 2 3; do printf . && sleep 0.3; done
 
 curl -N -s "http://localhost:$port/api/v1/namespaces/$namespace/pods?watch=true" |
   while read -r line; do
-    name=$(jq -r .object.metadata.name <<<"$line")
-    status=$(jq -r .object.status.phase <<<"$line")
-    case "$status" in Running) color=32;; Pending) color=33;; *) color=31;; esac
-    status=$(printf "\033[${color}m$status\033[0m")
+    name=$(jq -r '.object.metadata.name' <<<"$line")
+    phase=$(jq -r '.object.status.phase' <<<"$line")
+    is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | select(.type=="Ready").status else "False" end' <<<"$line")
+    [[ "$phase" = Running && "$is_ready" != True ]] && phase=Pending
+    case "$phase" in Running) color=32;; Pending) color=33;; *) color=31;; esac
+    phase=$(printf "\033[${color}m$phase\033[0m")
     case $(jq -r .type <<<"$line") in
-      ADDED) echo "$name $status" >>"$file" ;;
-      MODIFIED) sed -i "s/^$name .*$/$name $status/" "$file" ;;
+      ADDED) echo "$name $phase" >>"$file" ;;
+      MODIFIED) sed -i "s/^$name .*$/$name $phase/" "$file" ;;
       DELETED) sed -i "/^$name .*$/d" "$file";;
     esac
   done &
