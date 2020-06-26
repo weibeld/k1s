1edda109a8409b0a4dc6af1b5d11d747dbe63275
Daniel Weibel <danielmweibel@gmail.com>
2020-06-26 13:12:16 +0200

    Add support for "all namespaces" and non-namespaced resources
    If the namespace command-line argument is "-" and a namespaced resource
    type is specified, then the resources across all namespaces are
    displayed.
    
    If the namespace argument is "-" and a non-namespaced resource type is
    specified (e.g. clusterroles), then all the resources of this type are
    displayed.
    
    If a non-namespaced resource type is specified (e.g. clusterroles) and
    the namespace command-line argument is not "-" (e.g. it's "default"),
    then this is detected and the namespace indicator in the dashboard
    banner is set to "-" (this detection works by counting the number of
    elements in the detected API path; if it's 3 or 4, the path does not
    include a namespace, if it's 5 or 6, the path includes a namespace).

--- k1s.sh
+++ k1s.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
-set -o pipefail
-namespace=${1:-default}
-resource_type=${2:-pods}
-
-printf Loading && while true; do printf . && sleep 0.1; done &
-if ! path=$(kubectl get "$resource_type" -n "$namespace" -v 6 2>&1 >/dev/null | grep GET | tail -n 1 | sed -n 's#.*https://[^/]*\([a-z0-9/.-]*\).*#\1#p'); then
-  kill -9 "$!" && echo -e "\nInvalid resource type: $resource_type" && exit 1
-fi
-kill -9 "$!" && wait "$!" 2>/dev/null
-resource_type=${path##*/}
-
-exec 3< <(kubectl proxy -p 0)
-port=$(head -n 1 <&3 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
+ns=${1:-default}
+res=${2:-pods}
 
 c() { echo -e "\033[$1m"; }
 cc() { echo -e "\033[$1;1m"; }
+
+printf Loading && while true; do printf . && sleep 0.1; done &
+
+set -o pipefail
+path=$(kubectl get "$res" "$([[ "$ns" = - ]] && echo --all-namespaces || echo -n=$ns)" -v 6 2>&1 >/dev/null | grep GET | tail -n 1 | sed -n 's#.*https://[^/]*\([a-z0-9/.-]*\).*#\1#p')
+pid=$?
+kill -9 "$!" && wait "$!" 2>/dev/null
+[[ "$pid" -ne 0 ]] && echo -e "\nInvalid resource type: $res" && exit 1
+[[ $(echo -n "${path//[^\/]}" | wc -c) -lt 5 ]] && ns=-
+res=${path##*/}
+
+exec 3< <(kubectl proxy -p 0)
+port=$(head -n 1 <&3 | sed 's/.*:\([0-9]\{4,5\}\)\b.*/\1/')
 
 file=$(mktemp)
 cat <<EOF >"$file"
 $(cc 36) ____ ____ ____
 ||$(cc 33)k$(cc 36) |||$(cc 33)1$(cc 36) |||$(cc 33)s$(cc 36) ||  $(cc 0)Kubernetes Dashboard$(cc 36)
-||__|||__|||__||  $(cc 0)Namespace: $namespace$(cc 36)
-|/__\|/__\|/__\|  $(cc 0)Resources: $resource_type$(cc 36)
-$(c 0)
+||__|||__|||__||  $(cc 0)Namespace: $ns$(cc 36)
+|/__\|/__\|/__\|  $(cc 0)Resources: $res$(c 0)
+
 EOF
 
 curl -N -s "http://localhost:$port$path?watch=true" |
-  while read -r line; do
-    name=$(jq -r '.object.metadata.name' <<<"$line")
-    case "$resource_type" in
+  while read -r event; do
+    name=$(jq -r '.object.metadata.name' <<<"$event")
+    case "$res" in
     pods)
-      phase=$(jq -r '.object.status.phase' <<<"$line")
-      is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | if select(.type=="Ready").status=="True" then "true" else "" end else "" end' <<<"$line")
-      is_scheduled=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | if select(.type=="PodScheduled").status=="True" then "true" else "" end else "" end' <<<"$line")
+      phase=$(jq -r '.object.status.phase' <<<"$event")
+      is_ready=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | if select(.type=="Ready").status=="True" then "1" else "" end else "" end' <<<"$event")
+      is_scheduled=$(jq -r 'if .object.status | has("conditions") then .object.status.conditions[] | if select(.type=="PodScheduled").status=="True" then "1" else "" end else "" end' <<<"$event")
       [[ "$is_scheduled" && ! "$is_ready" ]] && info=NonReady || info=$phase
       [[ "$info" = Running ]] && info=$(c 32)$info$(c 0) || info=$(c 33)$info$(c 0) ;;
     deployments|replicasets|statefulsets)
-      spec=$(jq -r '.object.spec.replicas' <<<"$line")
-      stat=$(jq -r '.object.status.readyReplicas // 0' <<<"$line")
+      spec=$(jq -r '.object.spec.replicas' <<<"$event")
+      stat=$(jq -r '.object.status.readyReplicas // 0' <<<"$event")
       [[ "$stat" = "$spec" ]] && info="$(c 32)($stat/$spec)$(c 0)" || info="$(c 33)($stat/$spec)$(c 0)" ;;
     esac
-    case $(jq -r .type <<<"$line") in
+    case $(jq -r .type <<<"$event") in
       ADDED) echo "$name $info" >>"$file" ;;
       MODIFIED) sed -i "s/^$name .*$/$name ${info//\//\\/}/" "$file" ;;
       DELETED) sed -i "/^$name .*$/d" "$file";;
